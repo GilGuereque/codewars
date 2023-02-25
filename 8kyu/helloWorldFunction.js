f9bd0349a6c9424ab315422e6dbdd0dc5bfc3a86
GilGuereque <gguereque326@gmail.com>
2023-02-25 15:53:47 -0800

    demo commit

--- 8kyu/helloWorldFunction.js
+++ 8kyu/helloWorldFunction.js
@@ -4,6 +4,7 @@ Style Points
 Sure, this is about as easy as it gets. 
 But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends? */
 
+//My solution:
 function greet(){
     let greeting = 'hello world!';
     return greeting;
