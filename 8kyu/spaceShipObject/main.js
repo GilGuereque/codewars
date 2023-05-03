076ce5ad2be9c74d269f744a05bc64941071ce1f
GilGuereque <gguereque326@gmail.com>
2023-05-02 19:43:56 -0700

    Codecademy spaceship objects

---
+++ 8kyu/spaceShipObject/main.js
@@ -0,0 +1,49 @@
+// codecademy spaceship objects module
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth'
+  };
+  
+  // Write your code below
+  let greenEnergy = obj => {
+    obj['Fuel Type'] = 'avocado oil';
+  };
+  
+  let remotelyDisable = obj => {
+    obj.disabled = true;
+  };
+  
+  greenEnergy(spaceship);
+  remotelyDisable(spaceship);
+  
+  console.log(spaceship);
+
+  // for loop through object
+  let spaceship = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+}; 
+
+// Write your code below
+for (let crewMember in spaceship.crew){
+  console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`);
+}
