71c0b0ef56902984d18de2fc67afc1a5a85f66ec
Gilberto Guereque <38387405+GilGuereque@users.noreply.github.com>
2023-05-04 09:55:27 -0700

    Create objectsPractice.js

---
+++ 8kyu/objectsPractice.js
@@ -0,0 +1,10 @@
+// Practicing Objects from Codecademy modules
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  provideInfo() {
+    return `I am ${this.model} and my current energy level is ${this.energyLevel}`;
+  }
+};
+
+console.log(robot.provideInfo());
